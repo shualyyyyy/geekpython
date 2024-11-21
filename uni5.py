77c0eaa5dd8b871b3034ad780a501f178bbb45fa
shualyyyyy <shualyyyy@gmail.com>
2024-11-21 13:08:08 +0300

    Delete uni5.py

--- uni5.py
+++
@@ -1,7 +0,0 @@
-var6 = ['разработка', 'администрирование', 'protocol', 'standard']
-for i in var6:
-    a = i.encode('utf-8')
-    print(a, type(a))
-    b = bytes.decode(a, 'utf-8')
-    print(b, type(b))
-    print('#'*15)
